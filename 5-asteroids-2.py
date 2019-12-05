36c01b41beb0bcf98fc6852abfa11a3fde84f635
Sean Bechhofer <sean.bechhofer@manchester.ac.uk>
2019-12-05 18:32:51 +0000

    Cleaned up and nicer solution.

--- 5-asteroids-2.py
+++ 5-asteroids-2.py
@@ -13,10 +13,20 @@ LESS_THAN = 7
 EQUALS = 8
 HALT = 99
 
+# Probably don't need args.
+LANG = {
+    ADD: {"steps": 4, "args": 3},
+    MULTIPLY: {"steps": 4, "args": 3},
+    WRITE: {"steps": 2, "args": 1},
+    OUTPUT: {"steps": 2, "args": 1},
+    JUMP_TRUE: {"steps": 3, "args": 2},
+    JUMP_FALSE: {"steps": 3, "args": 2},
+    LESS_THAN: {"steps": 4, "args": 3},
+    EQUALS: {"steps": 4, "args": 3},
+    HALT: {}
+}
+    
 # Steps to increment for each code
-STEPS = [0,4,4,2,2,3,3,4,4]
-ARGS = [0,3,3,1,1,2,2,3,3]
-
 def debug(arg):
     pass
     #print(arg)
@@ -25,12 +35,20 @@ def debug(arg):
 def split_instruction(opcode):
     instruction = opcode % 100
     parameter_modes = int(opcode / 100)
-    modes = [0,0,0]
-    for i in [0,1,2]:
-        modes[i] = parameter_modes % 10
+    modes = []
+    # Operands have no more that two referenceable arguments. 
+    for i in range(0,2):
+        modes.append(parameter_modes % 10)
         parameter_modes = int(parameter_modes / 10)
     return (instruction,modes)
 
+# Get the value given the counter, offset and access mode.
+def get_parameter(memory,counter,offset,mode):
+    val = memory[counter+offset]
+    if mode == 0:
+        return memory[val]
+    else:
+        return val
     
 # Execute the instruction according to the given state
 # Lot of duplication here. Could be much better!
@@ -45,28 +63,18 @@ def execute_instruction(state):
         return False
     
     if instruction == ADD or instruction == MULTIPLY or instruction == LESS_THAN or instruction == EQUALS:
-        arg1 = memory[counter+1]
-        arg2 = memory[counter+2]
-        arg3 = memory[counter+3]
-
-        if modes[0] != 0:
-            val1 = arg1
-        else:
-            val1 = memory[arg1]
-                    
-        if modes[1] != 0:
-            val2 = arg2
-        else:
-            val2 = memory[arg2]
+        arg1 = get_parameter(memory,counter,1,modes[0])
+        arg2 = get_parameter(memory,counter,2,modes[1])
+        arg3 = get_parameter(memory,counter,3,1)
 
         if instruction == ADD:
-            memory[arg3] = val1 + val2
+            memory[arg3] = arg1 + arg2
         if instruction == MULTIPLY:
-            memory[arg3] = val1 * val2
+            memory[arg3] = arg1 * arg2
         if instruction == LESS_THAN:
-            memory[arg3] = 1 if val1 < val2 else 0
+            memory[arg3] = 1 if arg1 < arg2 else 0
         if instruction == EQUALS:
-            memory[arg3] = 1 if val1 == val2 else 0
+            memory[arg3] = 1 if arg1 == arg2 else 0
 
     if instruction == WRITE:
         # Hard coded input!
@@ -74,38 +82,26 @@ def execute_instruction(state):
         memory[arg1] = 5
         
     if instruction == OUTPUT:
-        arg1 = memory[counter+1]
-        val1 = memory[arg1]
-        if modes[0] != 0:
-            val1 = arg1
-        print(val1)
+        arg1 = get_parameter(memory,counter,1,modes[0])
+        print(arg1)
 
     if instruction == JUMP_TRUE or instruction == JUMP_FALSE:
-        arg1 = memory[counter+1]
-        arg2 = memory[counter+2]
-        if modes[0] != 0:
-            val1 = arg1
-        else:
-            val1 = memory[arg1]
-                    
-        if modes[1] != 0:
-            val2 = arg2
-        else:
-            val2 = memory[arg2]
+        arg1 = get_parameter(memory,counter,1,modes[0])
+        arg2 = get_parameter(memory,counter,2,modes[1])
+            
         if instruction == JUMP_TRUE:
-            if val1 != 0:
-                state[1] = val2
+            if arg1 != 0:
+                state[1] = arg2
                 increment = False
         if instruction == JUMP_FALSE:
-            if val1 == 0:
-                state[1] = val2
+            if arg1 == 0:
+                state[1] = arg2
                 increment = False
 
     if increment:
-        state[1] = counter + STEPS[instruction]
+        state[1] = counter + LANG[instruction]['steps']
     return True
         
-
 if __name__=='__main__':
 #    unittest.main()
     memory = [int(s) for s in sys.stdin.readline().strip().split(",")]
