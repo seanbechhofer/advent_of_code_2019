b4f1b2c0b5cf7be7602833876a39e0d1760e40d7
Sean Bechhofer <sean.bechhofer@manchester.ac.uk>
2019-12-05 18:16:02 +0000

    Fixing broken solution

--- 5-asteroids-2.py
+++ 5-asteroids-2.py
@@ -19,80 +19,79 @@ ARGS = [0,3,3,1,1,2,2,3,3]
 
 def debug(arg):
     pass
-    #print arg
+    #print(arg)
 
 # Decipher the code and modes
 def split_instruction(opcode):
     instruction = opcode % 100
-    parameter_modes = opcode / 100
+    parameter_modes = int(opcode / 100)
     modes = [0,0,0]
-    modes[0] = parameter_modes % 10
-    parameter_modes = parameter_modes / 10
-    modes[1] = parameter_modes % 10
-    parameter_modes = parameter_modes / 10
-    modes[2] = parameter_modes % 10
-    parameter_modes = parameter_modes / 10
+    for i in [0,1,2]:
+        modes[i] = parameter_modes % 10
+        parameter_modes = int(parameter_modes / 10)
     return (instruction,modes)
 
+    
 # Execute the instruction according to the given state
 # Lot of duplication here. Could be much better!
 def execute_instruction(state):
-    prog = state[0]
+    memory = state[0]
     counter = state[1]
-    opcode = prog[counter]
+    opcode = memory[counter]
+
     increment = True
     (instruction,modes) = split_instruction(opcode)
     if instruction == HALT:
         return False
     
     if instruction == ADD or instruction == MULTIPLY or instruction == LESS_THAN or instruction == EQUALS:
-        arg1 = prog[counter+1]
-        arg2 = prog[counter+2]
-        arg3 = prog[counter+3]
+        arg1 = memory[counter+1]
+        arg2 = memory[counter+2]
+        arg3 = memory[counter+3]
 
         if modes[0] != 0:
             val1 = arg1
         else:
-            val1 = prog[arg1]
+            val1 = memory[arg1]
                     
         if modes[1] != 0:
             val2 = arg2
         else:
-            val2 = prog[arg2]
+            val2 = memory[arg2]
 
         if instruction == ADD:
-            prog[arg3] = val1 + val2
+            memory[arg3] = val1 + val2
         if instruction == MULTIPLY:
-            prog[arg3] = val1 * val2
+            memory[arg3] = val1 * val2
         if instruction == LESS_THAN:
-            prog[arg3] = 1 if val1 < val2 else 0
+            memory[arg3] = 1 if val1 < val2 else 0
         if instruction == EQUALS:
-            prog[arg3] = 1 if val1 == val2 else 0
+            memory[arg3] = 1 if val1 == val2 else 0
 
     if instruction == WRITE:
         # Hard coded input!
-        arg1 = prog[counter+1]
-
-        prog[arg1] = 5
+        arg1 = memory[counter+1]
+        memory[arg1] = 5
+        
     if instruction == OUTPUT:
-        arg1 = prog[counter+1]
-        val1 = prog[arg1]
+        arg1 = memory[counter+1]
+        val1 = memory[arg1]
         if modes[0] != 0:
             val1 = arg1
         print(val1)
 
     if instruction == JUMP_TRUE or instruction == JUMP_FALSE:
-        arg1 = prog[counter+1]
-        arg2 = prog[counter+2]
+        arg1 = memory[counter+1]
+        arg2 = memory[counter+2]
         if modes[0] != 0:
             val1 = arg1
         else:
-            val1 = prog[arg1]
+            val1 = memory[arg1]
                     
         if modes[1] != 0:
             val2 = arg2
         else:
-            val2 = prog[arg2]
+            val2 = memory[arg2]
         if instruction == JUMP_TRUE:
             if val1 != 0:
                 state[1] = val2
@@ -109,8 +108,8 @@ def execute_instruction(state):
 
 if __name__=='__main__':
 #    unittest.main()
-    prog = [int(s) for s in sys.stdin.readline().strip().split(",")]
+    memory = [int(s) for s in sys.stdin.readline().strip().split(",")]
     counter = 0
-    state = [prog,counter]
+    state = [memory,counter]
     while (execute_instruction(state)):
         pass
